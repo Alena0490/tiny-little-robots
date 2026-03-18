8700d317c4e7f547389305436e7ef5a3e174c065
Alena Pumprová <alenapumprova@seznam.cz>
2026-03-18 01:17:57 +0100

    robotic cat model reexport

--- src/components/Model.tsx
+++ src/components/Model.tsx
@@ -4,7 +4,7 @@ import { Canvas } from '@react-three/fiber'
 import type { GLTF } from 'three-stdlib'
 import { useGLTF, OrbitControls } from '@react-three/drei'
 import * as THREE from 'three'
-import modelPath from "../models/robotic_cat - kopie.glb"
+import modelPath from "../models/robotic_cat2.glb"
 import "./Model.css"
 
 type GLTFResult = GLTF & {
